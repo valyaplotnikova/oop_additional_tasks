01653ecc764662aa5f981541420e2e1230522a24
valya_plotnikova <valya_ezhova@mail.ru>
2024-04-22 13:36:39 +0300

    add class Bottle

--- lesson1/bottle.py
+++ lesson1/bottle.py
@@ -15,12 +15,13 @@
 class Bottle:
 
     def __init__(self, color, volume):
-        ...
+        self.color = color
+        self.volume = volume
 
 
 bottle_1 = Bottle("Красная", 0.7)
-bottle_2 = ...
-bottle_3 = ...
+bottle_2 = Bottle("Белая", 0.3)
+bottle_3 = Bottle("Черная", 1.0)
 
 
 # код для проверки 
