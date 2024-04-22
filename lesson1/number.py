21d492a78f4ee71115fbb4609f6e195fa1210cb5
valya_plotnikova <valya_ezhova@mail.ru>
2024-04-22 13:40:29 +0300

    add class number

--- lesson1/number.py
+++ lesson1/number.py
@@ -15,16 +15,16 @@
 class Number:
 
     def __init__(self, value):
-        ...
+        self.value = value
 
-    def ...(self):
-        ...
+    def get(self):
+        return self.value
 
-    def ...:
-        ...
+    def add(self, num):
+        self.value += num
 
-    def ...:
-        ...
+    def substract(self, num):
+        self.value -= num
 
 
 # код для проверки 
